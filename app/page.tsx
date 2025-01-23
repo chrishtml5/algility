16738cd53594cb1d7a2236ad1dd6c7626dd8d3b9
Christian Garcia <80602209+chrishtml5@users.noreply.github.com>
2025-01-22 23:59:43 -0600

    123

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ import confetti from 'canvas-confetti';
 const services = [
   {
     icon: "/images/s_6.png",
-    title: "AI Agents Development",
+    title: "AI Agent Development",
     description:
       "Take your business to the next level with our autonomous agent development services",
   },
