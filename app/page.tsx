534090312eafbd6b407e67d904b6bea2f9335ff2
Christian Garcia <80602209+chrishtml5@users.noreply.github.com>
2025-01-27 23:19:51 -0600

    Remove duplicate ScrollElement import

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import { InfiniteMovingLogos } from "@/components/ui/infinite-moving-logos";
 import Image from "next/image";
 import Link from "next/link";
 import { PiCheckBold } from "react-icons/pi";
-import { Link as ScrollLink, Element } from "react-scroll";
+import { Link as ScrollLink, Element as ScrollElement } from "react-scroll";
 import { IconStarFilled } from "@tabler/icons-react";
 import { ShootingStarsAndStarsBackgroundDemo } from "@/components/demos/shooting-stars-demo";
 import LetsMakeThingsHappenSection from "@/components/ui/lets-make-things-happen";
@@ -67,7 +67,7 @@ export default function Home() {
   bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)]
    bg-[size:14px_24px]"
     >
-      <Element
+      <ScrollElement
         name="top"
         className="overflow-hidden rounded-[6px] top-5 sticky md:mx-auto z-50 
    xl:w-4/5 2xl:w-[68%] bg-white flex items-center h-[80px]
@@ -144,7 +144,7 @@ export default function Home() {
             Book a call
           </Link>
         </div>
-      </Element>
+      </ScrollElement>
 
       <main className="md:pb-10">
         <div className="md:px-0 mx-6 xl:w-4/5 2xl:w-[68%] md:mx-auto mt-14">
@@ -270,7 +270,7 @@ export default function Home() {
         </div>
       </main>
 
-      <Element name="services">
+      <ScrollElement name="services">
         <div className="md:px-0 mx-6 xl:w-4/5 2xl:w-[68%] md:mx-auto ">
         <h1>
           <WordPullUpDemo />
@@ -298,13 +298,13 @@ export default function Home() {
             ))}
           </div>
         </div>
-      </Element>
+      </ScrollElement>
 
       <section className="py-20">
         <ScrollBasedVelocityDemo />
       </section>
 
-      <Element name="process">
+      <ScrollElement name="process">
         <main className="md:px-0 mx-6 md:mx-auto">
         <h1 className="text-3xl md:text-5xl md:text-center font-medium flex items-center gap-x-2 mx-auto justify-center">
         Our{" "}
@@ -347,7 +347,7 @@ export default function Home() {
           </div>
 
         </main>
-      </Element>
+      </ScrollElement>
 
       <section>
         <main className="md:flex items-center justify-center space-y-6 md:space-y-0 md:gap-x-20 xl:w-4/5 2xl:w-[68%] mx-auto px-6 md:px-0">
@@ -381,9 +381,9 @@ export default function Home() {
         </main>
       </section>
 
-      <Element name="guarentees">
+      <ScrollElement name="guarentees">
         <ShootingStarsAndStarsBackgroundDemo />
-      </Element>
+      </ScrollElement>
 
       <section className="my-10 md:py-20 xl:w-4/5 2xl:w-[68%] md:mx-auto">
       <LetsMakeThingsHappenSection />
